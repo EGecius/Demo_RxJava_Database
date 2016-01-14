dcf01651870e5368e08acb2eb1a7cd9187a8022c
Egidijus Gecius <egidijus.gecius@candyspace.com>
2016-01-14 17:17:04 +0000

    Initial commit

---
+++ app/src/main/java/db/Test.java
@@ -0,0 +1,82 @@
+package db;
+
+/**
+ * Single Responsibility:
+ *
+ * //todo
+ */
+public class Test {
+
+	// -------------- DEFINITIONS ------------
+	public static final String TABLE = "DontMiss";
+
+	public static final String ID_COLUMN_1 = "Id";
+	public static final int ID_COLUMN_POSITION = 1;
+
+	public static final String PROGRAMME_TITLE_COLUMN_2 = "ProgrammeTitle";
+	public static final int PROGRAMME_TITLE_COLUMN_POSITION = 2;
+
+	public static final String EPISODE_TITLE_COLUMN_3 = "EpisodeTitle";
+	public static final int EPISODE_TITLE_POSITION = 3;
+
+	public static final String SYNOPSIS_COLUMN_4 = "Owner";
+	public static final int SYNOPSIS_COLUMN_POSITION = 4;
+
+//	String getImageUrl();
+
+	public static final String IMAGE_URL_COLUMN_5 = "";
+	public static final int IMAGE_URL_COLUMN_POSITION = 5;
+
+//	String getChannel();
+
+	public static final String CHANNEL_COLUMN_6 = "";
+	public static final int CHANNEL_COLUMN_POSITION = 6;
+
+//		String getGenres();
+
+	public static final String GENRES_COLUMN_7 = "";
+	public static final int GENRES__COLUMN_POSITION = 7;
+
+//	int getDuration();
+
+	public static final String DURATION_COLUMN_8 = "";
+	public static final int DURATION_COLUMN_POSITION = 8;
+
+//	GregorianCalendar getLastBroadcastDate();
+
+	public static final String LAST_BROADCAST_DATE_COLUMN_9 = "";
+	public static final int LAST_BROADCAST_DATE_COLUMN_POSITION = 9;
+
+//	int getDaysRemaining();
+
+	public static final String DAYS_REMAINING_COLUMN_10 = "";
+	public static final int DAYS_REMAINING_COLUMN_POSITION = 10;
+
+//	String getParentalGuidanceWarning();
+
+
+	public static final String PARENTAL_GUIDANCE_WARNING_COLUMN_11 = "";
+	public static final int PARENTAL_GUIDANCE_WARNING_COLUMN_POSITION = 11;
+
+// ----	ProgrammeMetadata ----
+
+//	String getProgrammeId();
+
+
+	public static final String PROGRAMME_ID_COLUMN_12 = "";
+	public static final int PROGRAMME_ID_COLUMN_POSITION = 12;
+
+//		String getEpisodeId();
+
+
+	public static final String EPISODE_ID_COLUMN_13 = "";
+	public static final int EPISODE_ID_COLUMN_POSITION = 13;
+
+//	String getEpisodeProductionId();
+
+
+	public static final String EPISODE_PRODUCTION_ID_COLUMN_14 = "";
+	public static final int EPISODE_PRODUCTION_ID_COLUMN_POSITION = 14;
+
+
+}
